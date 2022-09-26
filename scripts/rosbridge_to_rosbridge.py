c9a7b9e7094813faec3a2ef5c6f6bba4a44ac5a6
MikhailBertrand <kenyu.kurimoto@gmail.com>
2022-09-26 12:55:23 +0900

    Fix python format

--- scripts/rosbridge_to_rosbridge.py
+++ scripts/rosbridge_to_rosbridge.py
@@ -4,51 +4,61 @@
 # import warnings
 # warnings.filterwarnings('ignore')
 
-import rospy
-from roslibpy import Ros, Topic
-import rospkg
-
-import time
-import yaml
 import re
+import time
 from functools import partial
+
+import rospkg
+import rospy
+import yaml
+from roslibpy import Ros
+from roslibpy import Topic
+
 
 class rosbridge_to_rosbridge():
     def __init__(self):
         # ROS bridge
-        self.local_sub   = {}
-        self.bridge_pub  = {}
-        rospack          = rospkg.RosPack()
-        conf             = yaml.safe_load(open(rospack.get_path('ros_to_rosbridge')+'/conf/config.yaml'))
-        topics           = rospy.get_published_topics('/')
+        self.local_sub = {}
+        self.bridge_pub = {}
+        rospack = rospkg.RosPack()
+        conf = yaml.safe_load(open(rospack.get_path('ros_to_rosbridge') + '/conf/config.yaml'))
+        topics = rospy.get_published_topics('/')
         topics_list_dict = []
         for topic in topics:
-            topics_list_dict.append({'name':topic[0], 'type':topic[1]})
-        use_id_for_ns   = bool(rospy.get_param('~use_id_for_ns', "False"))
+            topics_list_dict.append({'name': topic[0], 'type': topic[1]})
+        use_id_for_ns = bool(rospy.get_param('~use_id_for_ns', "False"))
 
-        self.local_ros_client  = Ros(rospy.get_param('~local_host','127.0.0.1'), rospy.get_param('~local_port', 9090))
-        self.bridge_ros_client = Ros(rospy.get_param('~remote_host','127.0.0.1'), rospy.get_param('~remote_port', 9090))
+        self.local_ros_client = Ros(rospy.get_param('~local_host', '127.0.0.1'), rospy.get_param('~local_port', 9090))
+        self.bridge_ros_client = Ros(
+            rospy.get_param(
+                '~remote_host', '127.0.0.1'), rospy.get_param(
+                '~remote_port', 9090))
 
         rospy.loginfo('')
-        rospy.loginfo('Connect Local Host : [%s:%s], Remote Host : [%s:%s]', rospy.get_param('~local_host','127.0.0.1'), rospy.get_param('~local_port', 9090),rospy.get_param('~remote_host','127.0.0.1'), rospy.get_param('~remote_port', 9090))
+        rospy.loginfo(
+            'Connect Local Host : [%s:%s], Remote Host : [%s:%s]', rospy.get_param(
+                '~local_host', '127.0.0.1'), rospy.get_param(
+                '~local_port', 9090), rospy.get_param(
+                '~remote_host', '127.0.0.1'), rospy.get_param(
+                    '~remote_port', 9090))
         rospy.loginfo('')
 
         rospy.loginfo('Make below topics bridge')
-        
+
         # set bridge subscriber & publisher
         def set_pub_sub(topicname, datatype):
             pub_topicname = topicname
-            if use_id_for_ns: pub_topicname = '/' + conf['id'] + topicname            
+            if use_id_for_ns:
+                pub_topicname = '/' + conf['id'] + topicname
             rospy.loginfo('Local Sub:[%s] => Bridge Pub:[%s]', topicname, pub_topicname)
 
-            self.local_sub[topicname]  = Topic(self.local_ros_client, topicname, datatype)
+            self.local_sub[topicname] = Topic(self.local_ros_client, topicname, datatype)
             self.bridge_pub[topicname] = Topic(self.bridge_ros_client, pub_topicname, datatype)
 
             callback = partial(self.callback, self.bridge_pub[topicname])
             self.local_sub[topicname].subscribe(callback)
 
-
-        if conf['include_topics']==None:
+        if conf['include_topics'] is None:
             # if match exclude topic => flag is True
             flag = False
             for topic_dict in topics_list_dict:
@@ -57,7 +67,7 @@ class rosbridge_to_rosbridge():
                     if flag:
                         break
                 if not flag:
-                    
+
                     set_pub_sub(topic_dict['name'], topic_dict['type'])
         else:
             for topic_conf in conf['include_topics']:
@@ -75,6 +85,7 @@ class rosbridge_to_rosbridge():
             pub.publish(message)
         else:
             rospy.loginfo('Disconnected')
+
 
 if __name__ == '__main__':
     rospy.init_node('rosbridge_to_rosbridge')
